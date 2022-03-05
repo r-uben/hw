9dc3abf69c82ebf7f02f1e5b5f42c0a413f6e4dd
rubenexojo1 <r.fernandezfuertes.math@gmail.com>
2022-03-05 12:37:52 +0100

    R matrix (df) is now built

--- code/table9.py
+++ code/table9.py
@@ -1,5 +1,6 @@
 from cmath import nan
 from imp import init_builtin
+from unicodedata import name
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
@@ -9,6 +10,9 @@ from aux_class import AuxClass
 class Table9(object):
 
     def __init__(self, master_file, rf_file, num_firms):
+        #self.master_df     = pd.read_excel(master_file, sheet_name="T1_ptfs", names=["dt", "LeadR", "MidR", "LagR", "Lead", "Mid", "Lag", "LL", "LLStrong", "mktrf", "smb", "hml"], dtype=str)
+        # self.df_rf  = pd.read_csv(rf_file, sep=";", names=["dtt", "v2", "v3", "v4", "rf"], dtype=str)
+
         #self.aux = AuxClass(master_file, rf_file)
         self.num_firms = num_firms
         self.df = {}
@@ -22,11 +26,7 @@ class Table9(object):
     
         # Estimations are done from 1972 to 2012
         self.init_year = 1972
-        self.last_year   = 2021
-
-
-        #self.master_df     = pd.read_excel(master_file, sheet_name="T1_ptfs", names=["dt", "LeadR", "MidR", "LagR", "Lead", "Mid", "Lag", "LL", "LLStrong", "mktrf", "smb", "hml"], dtype=str)
-        # self.df_rf  = pd.read_csv(rf_file, sep=";", names=["dtt", "v2", "v3", "v4", "rf"], dtype=str)
+        self.last_year   = 2012
 
     def _df_set_time_frame(self):
         for num in self.num_firms:
@@ -34,29 +34,33 @@ class Table9(object):
             end_index   = self.dates[num].index(str(self.last_year) + "-12") 
             self.df[num] = self.df[num][init_index:end_index]
 
+    def _df_remove_invalid_pfs(self):
+        for num in self.num_firms:
+            N = len(self.df[num].columns)
+            T = len(self.df[num].index)
+            for n in range(1,N+1):
+                mean = np.mean(self.df[num][n])
+                for i in self.df[num].index.tolist():
+                    if self.df[num][n][i] == 0 or self.df[num][n][i] == nan:
+                        self.df[num][n][i] = mean
 
-    def _df_remove_invalid(self):
+    def _df_build_return_dataset(self):
         df = self.df[30]
-        N = len(df.columns)
-        T = len(df.index)
-        for n in range(1,N+1):
-            mean = np.mean(df[n])
-            for i in df.index.tolist():
-                if df[n][i] == 0 or df[n][i] == nan:
-                    df[n][i] = mean
-    
-    def _df_calculate_returns(self):
-        self._df_remove_invalid()
-        df = self.df[30]
-        R = {}
+        T = df.index.tolist()
+        R = {'dt' : T[1:]}
         for n in df.columns:
-            T = df[n].index.tolist()[:-2]
-            print(T)
-            for t in T:
-                R[t+1] = df[n][t+1] / df[n][t]
+            R[n] = []
+            for t in T[:-1]:
+                Rtt = df[n][T[T.index(t)+1]] / df[n][t]
+                R[n].append(Rtt)
+        R = pd.DataFrame(R).set_index('dt')
+        R = R.rename_axis(index=None, columns=None)
+        print(R.head())
 
-        R = pd.DataFrame(R)
-        print(R)
     def Table9(self):
-        #self._df_calculate_returns()
+        # Take the correct time frame
         self._df_set_time_frame()
+        # Remove every invalid pff (0 and NaN)
+        self._df_remove_invalid_pfs()
+        # Build Return data frame
+        self._df_build_return_dataset()
