e69320fdd7d26cf5529e4ff4debea8ae5da6a277
rubenexojo1 <r.fernandezfuertes.math@gmail.com>
2022-03-05 11:52:48 +0100

    Starting T9

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
+from doctest import master
 from table1 import Table1
 from table3 import Table3
 from table7 import Table7
+from table9 import Table9
 
 def find_file(name):
         return "data/" + name
@@ -9,9 +11,11 @@ if __name__=="__main__":
 
     master_file  = find_file("Data_master.xlsx")
     rf_file      = find_file("FF4_monthlyKF.csv")
-    T1           = Table1(master_file, rf_file, 24)
-    T1.Table1()
-    T3           = Table3(master_file, rf_file, 12)
-    T3.Table3()
-    T7           = Table7(master_file, rf_file, 12)
-    T7.Table7()
+#     T1           = Table1(master_file, rf_file, 24)
+#     T1.Table1()
+#     T3           = Table3(master_file, rf_file, 12)
+#     T3.Table3()
+#     T7           = Table7(master_file, rf_file, 12)
+#     T7.Table7()
+    T9          = Table9(master_file, rf_file, [30, 38, 49])
+    T9.Table9()
--- code/table9.py
+++ code/table9.py
@@ -1,3 +1,6 @@
+from cmath import nan
+from imp import init_builtin
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 from statsmodels.iolib.summary2 import summary_col
@@ -5,18 +8,55 @@ from aux_class import AuxClass
 
 class Table9(object):
 
-    def __init__(self, master_file, rf_file, lags):
-        self.aux = AuxClass(master_file, rf_file)
+    def __init__(self, master_file, rf_file, num_firms):
+        #self.aux = AuxClass(master_file, rf_file)
+        self.num_firms = num_firms
+        self.df = {}
+        self.dates = {}
+        for num in num_firms:
+            header = [n for n in range(1, num+1)]
+            self.df[num] = pd.read_csv("data/"+f"{num}"+"_industry_pfs.csv", sep=";", names= header)
+            # fix date format.
+            self.df[num].index = pd.to_datetime(self.df[num].index, format="%Y%m").strftime("%Y-%m")
+            self.dates[num]    = self.df[num].index.tolist()
+    
+        # Estimations are done from 1972 to 2012
+        self.init_year = 1972
+        self.last_year   = 2021
 
-        # self.df     = pd.read_excel(master_file, sheet_name="T1_ptfs", names=["dt", "LeadR", "MidR", "LagR", "Lead", "Mid", "Lag", "LL", "LLStrong", "mktrf", "smb", "hml"], dtype=str)
+
+        #self.master_df     = pd.read_excel(master_file, sheet_name="T1_ptfs", names=["dt", "LeadR", "MidR", "LagR", "Lead", "Mid", "Lag", "LL", "LLStrong", "mktrf", "smb", "hml"], dtype=str)
         # self.df_rf  = pd.read_csv(rf_file, sep=";", names=["dtt", "v2", "v3", "v4", "rf"], dtype=str)
-        #
-        self.ann    = 12
-        self.lags   = lags
-        # Factors
-        self.F      = self.df.keys().tolist()
-        self.K      = len(self.F)
-        self.zip_f  = {}
-        self.df_rf  = self.df_rf[["dtt", "rf"]]
-        self.df2    = self.df_rf.copy()
-        self.T      = len(self.df)
+
+    def _df_set_time_frame(self):
+        for num in self.num_firms:
+            init_index  = self.dates[num].index(str(self.init_year) + "-01")
+            end_index   = self.dates[num].index(str(self.last_year) + "-12") 
+            self.df[num] = self.df[num][init_index:end_index]
+
+
+    def _df_remove_invalid(self):
+        df = self.df[30]
+        N = len(df.columns)
+        T = len(df.index)
+        for n in range(1,N+1):
+            mean = np.mean(df[n])
+            for i in df.index.tolist():
+                if df[n][i] == 0 or df[n][i] == nan:
+                    df[n][i] = mean
+    
+    def _df_calculate_returns(self):
+        self._df_remove_invalid()
+        df = self.df[30]
+        R = {}
+        for n in df.columns:
+            T = df[n].index.tolist()[:-2]
+            print(T)
+            for t in T:
+                R[t+1] = df[n][t+1] / df[n][t]
+
+        R = pd.DataFrame(R)
+        print(R)
+    def Table9(self):
+        #self._df_calculate_returns()
+        self._df_set_time_frame()
