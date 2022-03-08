153975f226afbb4e7479decf12f5c2c0e36f6b95
rubenexojo1 <r.fernandezfuertes.math@gmail.com>
2022-03-08 21:05:39 +0100

    Fancy stuff in the main

--- code/main.py
+++ code/main.py
@@ -1,4 +1,3 @@
-from doctest import master
 from table1 import Table1
 from table3 import Table3
 from table7 import Table7
@@ -9,13 +8,26 @@ def find_file(name):
 
 if __name__=="__main__":
 
+    ### MAIN DATASETS.
+
     master_file     = find_file("Data_master.xlsx")
     monthly_factors = find_file("FF4_monthlyKF.csv")
-#     T1           = Table1(master_file, monthly_factors, 24)
-#     T1.Table1()
-#     T3           = Table3(master_file, rf_file, 12)
-#     T3.Table3()
-#     T7           = Table7(master_file, rf_file, 12)
-#     T7.Table7()
-    T9          = Table9(master_file, monthly_factors, [30, 38, 49])
+
+    ### TABLE 1 ###
+    lags        = 24
+    T1          = Table1(master_file, monthly_factors, lags)
+    T1.Table1()
+
+    ### TABLE 3 ###
+    lags        = 12
+    T3          = Table3(master_file, monthly_factors, lags)
+    T3.Table3()
+
+    ### TABLE 7 ###
+    T7          = Table7(master_file, monthly_factors, lags)
+    T7.Table7()
+
+    ### TABLE 9 ###
+    num_firms = [30, 38, 49]
+    T9          = Table9(master_file, monthly_factors, num_firms)
     T9.Table9()
